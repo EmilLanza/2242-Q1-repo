3068645ee4f4b7ec1844359368e469945f640417
Emil F. Lanza <2019120200@gmail.com>
2023-01-29 23:53:54 -0600

    Main for Circle

--- circle.go
+++ circle.go
@@ -1,9 +1,29 @@
 package main
 
-type circle struct {
+import (
+	"fmt"
+)
+
+type Circle struct {
 	radius float64
 }
 
-func (c circle) area() float64 {
-	return 3.14 * (radius * radius)
+func (c Circle) Area() float64 {
+	return 3.14 * (c.radius * c.radius)
+}
+
+func (c Circle) Perimeter() float64 {
+	return 2 * 3.14 * c.radius
+}
+
+func main() {
+	area := Circle{
+		radius: 3,
+	}
+
+	perimeter := Circle{
+		radius: 7,
+	}
+
+	fmt.Println("Area of circle", area.Area(), "peremeter", perimeter.Perimeter())
 }
